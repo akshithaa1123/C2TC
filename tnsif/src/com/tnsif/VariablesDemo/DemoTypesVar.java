6d3041f186fa9c5a5c36343a61faa990a4c26545
AKSHITHA <akshitha.ananthala123@gmail.com>
2025-05-07 22:03:01 +0530

    Types of Variables

---
+++ tnsif/src/com/tnsif/VariablesDemo/DemoTypesVar.java
@@ -0,0 +1,16 @@
+package com.tnsif.VariablesDemo;
+
+public class DemoTypesVar {
+	    //instance variable
+		int numone;
+		//method
+		void display() {
+			//local variable
+		     int numtwo=20;
+			System.out.println("This is local variable "+numtwo);
+		}
+		//static variable
+		static int numthree=30;
+}
+
+
