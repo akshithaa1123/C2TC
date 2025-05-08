93973d1dc291ade2b6088198dc238b93a593ccfc
AKSHITHA <akshitha.ananthala123@gmail.com>
2025-05-08 09:40:31 +0530

    student

---
+++ tnsif/src/com/tnsif/DemoTypesVar/Student.java
@@ -0,0 +1,10 @@
+package com.tnsif.DemoTypesVar;
+
+public class Student {
+	int sid;
+	String sname;
+	static String collegename="Sri indu";
+	 void display() {
+		 System.out.println("The details of the student"+sid+","+sname+","+collegename);
+}
+}
